92298075ac80bbc8a52107878cb5be1a14f088fa
Fadi Ksho <fadiksho@gmail.com>
2019-03-04 20:58:10 +0100

    Intial MVC

---
+++ ASPNETCore_Assignments/Controllers/Home.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore_Assignments.Controllers
+{
+  public class Home : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      return View();
+    }
+  }
+}
