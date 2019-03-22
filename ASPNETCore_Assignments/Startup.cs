c451b243ca018b9bff3c9a0fee1ce5876e69db54
Fadi Ksho <fadiksho@gmail.com>
2019-03-22 13:15:42 +0100

    Configure startup to use appsetting and DbContext using sqlServer provider.

--- ASPNETCore_Assignments/Startup.cs
+++ ASPNETCore_Assignments/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using ASPNETCore_Assignments.Persistence;
+using ASPNETCore_Assignments.Persistence.Data;
 using ASPNETCore_Assignments.Reository;
+using ASPNETCore_Assignments.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -9,10 +13,22 @@ namespace ASPNETCore_Assignments
 {
   public class Startup
   {
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
     {
+			var appSetting = Configuration.Get<AppSettings>();
+
+			services.Configure<AppSettings>(Configuration);
+			services.AddDbContext<SchoolManagementContext>(options =>
+					options.UseSqlServer(appSetting.ConnectionStrings.DefaultConnection));
 			services.AddDistributedMemoryCache();
 			services.AddSession(options =>
 			{
