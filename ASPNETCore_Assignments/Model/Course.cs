f253ec8166eb34b3a5fc22a8e0ca04701952c38e
Fadi Ksho <fadiksho@gmail.com>
2019-03-24 18:40:24 +0100

    Adding model class for the school mangemnet.

---
+++ ASPNETCore_Assignments/Model/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore_Assignments.Model
+{
+	public class Course
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public Teacher Teacher { get; set; }
+
+		public List<CourseAssignment> CourseAssignments { get; set; }
+			= new List<CourseAssignment>();
+		public List<Student> StudentCourses { get; set; }
+			= new List<Student>();
+	}
+}
---
+++ ASPNETCore_Assignments/Model/CourseAssignment.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNETCore_Assignments.Model
+{
+	public class CourseAssignment
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+		public bool IsDone { get; set; }
+
+		public Course Course { get; set; }
+	}
+}
